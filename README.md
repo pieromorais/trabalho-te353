1db5812fdc143ffb7e2235cd042b3fd6016f52ba
Piero Morais <piero.morais@gmail.com>
2020-12-21 09:08:53 -0300

    Alteração do Readme e do main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # trabalho-te353
 Página do github para colaboração para desenvolver o trabalho de programação orientada à objetos.
+
+Loja de informática em programação C++ com os paradigmas de orientação à objetos.
+Data para entrega: 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
+/*trabalho te353
+Loja de informática
+Alunos:
+Alysson
+Eduardo
+Felipe
+Piero
+Vinícius*/
+
 #include<iostream>
 
 int main(int argc, char* argv[]){
-    std::cout << "Hello World!" << "\n";
+    std::cout << "Loja de informatica" << "\n";
 
     return 0;
 }
